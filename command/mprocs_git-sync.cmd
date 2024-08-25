e5574e4af90311ef9105cc175b698263bd3fd7df
scillidan <scillidan@yandex.com>
2024-08-25 11:41:52 +0800

    1

--- command/mprocs_git-sync.cmd
+++ command/mprocs_git-sync.cmd
@@ -1,10 +1,9 @@
 C:\Users\User\Bin\mprocs\mprocs.exe ^
-	"cd C:\Users\User\Github\_\Cos_Asset && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\Cos_Asset_Byya && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\Cos_Cache && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\Cos_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\My_Backup && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\My_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\My_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\My_Picture && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\_\My_Screenshot && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s"
+	"cd C:\Users\User\Mnt\github\Cos_Asset && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\Cos_Cache && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\Cos_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\Cos_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\My_Backup && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\My_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\My_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Mnt\github\My_Shell && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s"
