cea8204adf93abfe20eb71ae8be420681fb2a8e2
scillidan <scillidan@yandex.com>
2024-09-24 19:04:32 +0800

    changes from i7-14700KF on Tue, Sep 24, 2024 7:04:31 PM

--- command/mprocs_git-sync.cmd
+++ command/mprocs_git-sync.cmd
@@ -1,9 +1,9 @@
 C:\Users\User\Bin\mprocs\mprocs.exe ^
-	"cd C:\Users\User\Github\Cos_Asset && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\Cos_Media && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\Cos_Cache && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\Cos_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
-	"cd C:\Users\User\Github\Cos_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\mnt\github\Cos_Asset && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\mnt\github\Cos_Media && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\mnt\github\Cos_Cache && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\mnt\github\Cos_Document && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\mnt\github\Cos_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\My_Backup && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\My_Data && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\My_Shell && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
