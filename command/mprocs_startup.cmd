3167ebb132aa17baac16971abd0d5bbf90539587
scillidan <scillidan@yandex.com>
2024-09-28 16:59:03 +0800

    changes from i7-14700KF on Sat, Sep 28, 2024 4:59:02 PM

--- command/mprocs_startup.cmd
+++ command/mprocs_startup.cmd
@@ -3,6 +3,7 @@ C:\Users\User\Bin\mprocs\mprocs.exe ^
 	"C:\Users\User\Source\autohotkey\ahk-keyboard-locker\keyboard-locker.ahk" ^
   "C:\Users\User\Source\autohotkey\ahk-resize-windows\resize.ah2" ^
 	"C:\Users\User\Source\autohotkey\QuickSwitch\QuickSwitch.ahk" ^
+  "C:\Users\User\Source\autohotkey\AHK-KeyMap\KeyMap_User.ahk" ^
 	"C:\Users\User\Source\autohotkey\ShortScript\ShortScript.ahk" ^
 	"C:\Users\User\Source\goldendict\GoldenDictOCR\GoldenDictOCR.ahk" ^
 	"C:\Users\User\Source\autohotkey\Autocomplete\Autocomplete_User.ahk" ^
