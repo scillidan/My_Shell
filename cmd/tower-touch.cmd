bc623686dfe667451bd4a98171840db12172a941
scillidan <scillidan@yandex.com>
2024-12-22 21:38:09 +0800

    changes from i7-14700KF on Sun, Dec 22, 2024 9:38:08 PM

--- cmd/tower-touch.cmd
+++ cmd/tower-touch.cmd
@@ -1,5 +1,5 @@
 move %2.%3 %1_%2.%3 ^
 	&& touch %1_%2.%3.meta ^
 	&& echo-cli "{\n\"Title\": \"%2\"\n}" > %1_%2.%3.meta ^
-	&& move %1_%2.%3 C:\Users\User\Github\BYYA-site\content.zh\docs\tower\tower\%1_%2.%3 ^
+	&&  %1_%2.%3 C:\Users\User\Github\BYYA-site\content.zh\docs\tower\tower\%1_%2.%3.png ^
 	&& move %1_%2.%3.meta C:\Users\User\Github\BYYA-site\content.zh\docs\tower\tower\%1_%2.%3.meta
