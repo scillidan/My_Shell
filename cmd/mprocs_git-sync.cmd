52160c2fbd0188f8b5862a5b86bd616dcac424f2
scillidan <scillidan@yandex.com>
2024-12-29 21:42:18 +0800

    changes from i7-14700KF on Sun, Dec 29, 2024 9:42:16 PM

--- cmd/mprocs_git-sync.cmd
+++ cmd/mprocs_git-sync.cmd
@@ -8,6 +8,7 @@ C:\Users\User\Bin\mprocs\mprocs.exe ^
 	"cd C:\Users\User\Github\My_Shell && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\My_Store && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\My_Table && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
+	"cd C:\Users\User\Github\_\table && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\_\asset && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\_\gpt_share && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s" ^
 	"cd C:\Users\User\Github\_\images && C:\Users\User\Bin\Git\bin\bash.exe C:\Users\User\Bin\git-sync\git-sync -n -s"
