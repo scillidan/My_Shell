4ca6ad0bf58478e864213e5bfa41dfa0d246d5a8
scillidan <scillidan@yandex.com>
2025-01-09 17:07:03 +0800

    changes from 6900h-es-revios on Thu, Jan 9, 2025 5:07:02 PM

--- cmd/mprocs_startup_cache.cmd
+++ cmd/mprocs_startup_cache.cmd
@@ -1,9 +1,9 @@
 C:\Users\User\Bin\mprocs\mprocs.exe ^
   "C:\Users\User\Source\autohotkey\AHK-KeyMap\KeyMap_User.ahk" ^
 	"C:\Users\User\Source\autohotkey\QuickSwitch\QuickSwitch.ahk" ^
+	"C:\Users\User\Source\goldendict\GoldenDictOCR\GoldenDictOCR.ahk" ^
+	"C:\Users\User\Source\autohotkey\ahk-keyboard-locker\keyboard-locker.ahk" ^
 	"C:\Users\User\Github\My_Setup\autohotkey\User.ahk"
 	rem "C:\Users\User\Source\autohotkey\toggle-screen-autohotkey\Toggle^ screen.ahk" ^
-	rem "C:\Users\User\Source\autohotkey\ahk-keyboard-locker\keyboard-locker.ahk" ^
 	rem "C:\Users\User\Source\autohotkey\ShortScript\ShortScript.ahk" ^
-	rem "C:\Users\User\Source\goldendict\GoldenDictOCR\GoldenDictOCR.ahk" ^
 	rem "C:\Users\User\Source\autohotkey\AutoHotKey_MouseGesture\MG_User_User.ahk" ^
